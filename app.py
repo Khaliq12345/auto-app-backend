f299cbebac5d32a1490c993733020ec08de244aa
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-04-06 12:15:48 +0100

    Authentication added

--- app.py
+++ app.py
@@ -60,54 +60,105 @@ def login(client: Annotated[Client, Depends(get_session)], email: str, password:
 
 @app.get("/get_all_cars")
 def get_all_cars(
-    client: Annotated[Client, Depends(get_session)], page: int = 0, limit: int = 20
+    access_token: str,
+    refresh_token: str,
+    client: Annotated[Client, Depends(get_session)],
+    page: int = 0,
+    limit: int = 20,
 ):
     try:
+        auth = client.auth.set_session(
+            access_token=access_token,
+            refresh_token=refresh_token,
+        )
         response = (
             client.table("Vehicles").select("*").limit(limit).offset(page).execute()
         )
-        return jsonable_encoder(response)
+        return {
+            "session": jsonable_encoder(auth),
+            "details": jsonable_encoder(response),
+        }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    except AuthApiError:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
 
 
 @app.get("/get_car_comparisons")
-def get_car_comparisons(client: Annotated[Client, Depends(get_session)], car_id: str):
+def get_car_comparisons(
+    access_token: str,
+    refresh_token: str,
+    client: Annotated[Client, Depends(get_session)],
+    car_id: str,
+):
     try:
+        auth = client.auth.set_session(
+            access_token=access_token,
+            refresh_token=refresh_token,
+        )
         response = (
             client.table("comparisons")
             .select("*")
             .eq("parent_car_id", car_id)
             .execute()
         )
-        return jsonable_encoder(response)
+        return {
+            "session": jsonable_encoder(auth),
+            "details": jsonable_encoder(response),
+        }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    except AuthApiError:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
 
 
 @app.get("/scrape_status")
-def get_status(client: Annotated[Client, Depends(get_session)]):
+def get_status(
+    access_token: str,
+    refresh_token: str,
+    client: Annotated[Client, Depends(get_session)],
+):
     try:
+        auth = client.auth.set_session(
+            access_token=access_token,
+            refresh_token=refresh_token,
+        )
         response = client.table("Status").select("*").execute()
-        return jsonable_encoder(response)
+        return {
+            "session": jsonable_encoder(auth),
+            "details": jsonable_encoder(response),
+        }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    except AuthApiError:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
 
 
 @app.get("/start_scraping")
 def get_start_scraping(
+    access_token: str,
+    refresh_token: str,
     background_task: BackgroundTasks,
     client: Annotated[Client, Depends(get_session)],
     dev: bool = True,
 ):
     try:
+        auth = client.auth.set_session(
+            access_token=access_token,
+            refresh_token=refresh_token,
+        )
         background_task.add_task(start_services, client, dev)
-        return {"message": "Scraping started"}
+        return {
+            "message": "Scraping started",
+            "session": jsonable_encoder(auth),
+        }
     except Exception as e:
         client.table("Status").upsert(
             {"id": 1, "status": "failed", "stopped_at": datetime.now().isoformat()}
         ).execute()
         raise HTTPException(status_code=500, detail=str(e))
+    except AuthApiError:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
 
 
 if __name__ == "__main__":
