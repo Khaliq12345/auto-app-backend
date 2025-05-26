ad05adf124a31ff03ea6ae56c4a8239271005cda
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-05-26 14:08:58 +0100

    price difference calculated

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from services import autoscout24, lacentrale
 import pandas as pd
 from utilities import utils
-from fastapi import FastAPI, Depends, HTTPException, BackgroundTasks, UploadFile
+from fastapi import (
+    FastAPI,
+    Depends,
+    HTTPException,
+    BackgroundTasks,
+    UploadFile,
+)
 from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Annotated
-from supabase import Client, AuthApiError, create_client
+from supabase import (
+    Client,
+    AuthApiError,
+    create_client,
+)
 from datetime import datetime
 import os
 import aiofiles
@@ -23,15 +33,21 @@ domain_functions = {
 }
 
 
-def get_avg_price_based_on_domain(records: list[dict], percentage_limit: int = 95):
+def get_avg_price_based_on_domain(
+    records: list[dict],
+    percentage_limit: int = 95,
+):
     # Get the average price based on the best matching percentage for each domain
     best_prices = []
     for domain in domains:
         for record in records:
             if (record["domain"] == domain) and (
-                record["matching_percentage"] >= percentage_limit
+                record["matching_percentage"]
+                >= percentage_limit
             ):
-                best_prices.append(record["price"])
+                best_prices.append(
+                    record["price"]
+                )
     best_prices = [bp for bp in best_prices if bp]
     if best_prices:
         return sum(best_prices) / len(best_prices)
@@ -41,14 +57,20 @@ def get_avg_price_based_on_domain(records: list[dict], percentage_limit: int = 9
 
 def get_session() -> Client:
     client: Client = create_client(
-        supabase_key=utils.supabase_key, supabase_url=utils.url
+        supabase_key=utils.supabase_key,
+        supabase_url=utils.url,
     )
     return client
 
 
 def check_if_id_supabase(row_id: str) -> bool:
     client = get_session()
-    response = client.table("Vehicles").select("id").eq("id", row_id).execute()
+    response = (
+        client.table("Vehicles")
+        .select("id")
+        .eq("id", row_id)
+        .execute()
+    )
     if response.data:
         return True
     return False
@@ -64,36 +86,66 @@ def start_services(
     client = get_session()
     try:
         if dev:
-            df = pd.read_excel(OUT_FILE, header=None).sample(10)
+            df = pd.read_excel(
+                OUT_FILE, header=None
+            ).sample(10)
         else:
-            df = pd.read_excel(OUT_FILE, header=None)
-            print(f'Total: {len(df)}')
+            df = pd.read_excel(
+                OUT_FILE, header=None
+            )
+            print(f"Total: {len(df)}")
 
         new_columns = []
         for col in df.columns.to_list():
-            new_columns.append(utils.numeric_to_alphabetic_column_name(col))
+            new_columns.append(
+                utils.numeric_to_alphabetic_column_name(
+                    int(col)
+                )
+            )
         df.columns = new_columns
         df.fillna(value=0, inplace=True)
         for row_id in range(len(df)):
-            car_dict = utils.get_row_dict(df, row_id)
+            car_dict = utils.get_row_dict(
+                df, row_id
+            )
             print(f"Car Info - {car_dict}")
 
-            if (check_if_id_supabase(car_dict["id"]) is True) and (ignore_old is True):
-                print(f"Already scraped! -> {car_dict['id']}")
+            if (
+                check_if_id_supabase(
+                    car_dict["id"]
+                )
+                is True
+            ) and (ignore_old is True):
+                print(
+                    f"Already scraped! -> {car_dict['id']}"
+                )
                 continue
 
             # Verifying which to scrape from and submitting the tasks to the thread pool
-            with ThreadPoolExecutor(max_workers=2) as executor:
+            with ThreadPoolExecutor(
+                max_workers=2
+            ) as executor:
                 if not sites_to_scrape:
                     for func in domain_functions:
                         executor.submit(
-                            domain_functions[func], car_dict, mileage_plus_minus
+                            domain_functions[
+                                func
+                            ],
+                            car_dict,
+                            mileage_plus_minus,
                         )
                 else:
-                    for site_to_scrape in sites_to_scrape:
-                        if site_to_scrape in domain_functions:
+                    for (
+                        site_to_scrape
+                    ) in sites_to_scrape:
+                        if (
+                            site_to_scrape
+                            in domain_functions
+                        ):
                             executor.submit(
-                                domain_functions[site_to_scrape],
+                                domain_functions[
+                                    site_to_scrape
+                                ],
                                 car_dict,
                                 mileage_plus_minus,
                             )
@@ -101,7 +153,9 @@ def start_services(
             # verify status
             if (row_id + 1) == len(df):
                 stats = "success"
-                stopped_at = datetime.now().isoformat()
+                stopped_at = (
+                    datetime.now().isoformat()
+                )
             else:
                 stopped_at = None
                 stats = "running"
@@ -116,9 +170,13 @@ def start_services(
                 }
             ).eq("id", 1).execute()
     except Exception as e:
-        print(f'Error: {e}')
+        print(f"Error: {e}")
         client.table("Status").update(
-            {"id": 1, "status": "failed", "stopped_at": datetime.now().isoformat()}
+            {
+                "id": 1,
+                "status": "failed",
+                "stopped_at": datetime.now().isoformat(),
+            }
         ).eq("id", 1).execute()
         raise e
 
@@ -135,25 +193,44 @@ app.add_middleware(
 
 
 @app.post("/login")
-def login(client: Annotated[Client, Depends(get_session)], email: str, password: str):
+def login(
+    client: Annotated[
+        Client, Depends(get_session)
+    ],
+    email: str,
+    password: str,
+):
     try:
-        response = client.auth.sign_in_with_password(
-            credentials={"email": email, "password": password}
+        response = (
+            client.auth.sign_in_with_password(
+                credentials={
+                    "email": email,
+                    "password": password,
+                }
+            )
         )
-        return {"message": "Login successful", "details": jsonable_encoder(response)}
+        return {
+            "message": "Login successful",
+            "details": jsonable_encoder(response),
+        }
     except AuthApiError:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials",
+        )
 
 
 @app.get("/get_all_cars")
 def get_all_cars(
     access_token: str,
     refresh_token: str,
-    client: Annotated[Client, Depends(get_session)],
+    client: Annotated[
+        Client, Depends(get_session)
+    ],
     page: int = 0,
     limit: int = 20,
     cut_off_price: int = 500,
-    domain: str|None = None,
+    domain: str | None = None,
     percentage_limit: int = 95,
 ):
     try:
@@ -166,45 +243,89 @@ def get_all_cars(
             .select("*, comparisons(*)")
             .limit(limit)
             .offset(page)
-            .order("matching_percentage", desc=True, foreign_table="comparisons")
+            .order(
+                "matching_percentage",
+                desc=True,
+                foreign_table="comparisons",
+            )
         )
         if domain:
-            stmt = stmt.eq("comparisons.domain", domain)
+            stmt = stmt.eq(
+                "comparisons.domain", domain
+            )
         response = stmt.execute()
         vehicles = []
         for vehicle in response.data:
-            comparison_prices = [x["price"] for x in vehicle["comparisons"]]
-            comparison_prices = [0] if not comparison_prices else comparison_prices
-            vehicle["lowest_price"] = min(comparison_prices)
-            vehicle["average_price"] = sum(comparison_prices) / len(comparison_prices)
-
-            avg_price = get_avg_price_based_on_domain(
-                vehicle["comparisons"], percentage_limit
+            comparison_prices = [
+                x["price"]
+                for x in vehicle["comparisons"]
+            ]
+            comparison_prices = (
+                [0]
+                if not comparison_prices
+                else comparison_prices
             )
-            vehicle["average_price_based_on_best_match"] = avg_price
-            if vehicle["price_with_tax"] < avg_price:
+            vehicle["lowest_price"] = min(
+                comparison_prices
+            )
+            vehicle["average_price"] = sum(
+                comparison_prices
+            ) / len(comparison_prices)
+            avg_price = (
+                get_avg_price_based_on_domain(
+                    vehicle["comparisons"],
+                    percentage_limit,
+                )
+            )
+            vehicle[
+                "average_price_based_on_best_match"
+            ] = avg_price
+            vehicle[
+                "price_difference_with_avg_price"
+            ] = (
+                avg_price
+                - vehicle["price_with_tax"]
+            )
+
+            if (
+                vehicle["price_with_tax"]
+                < avg_price
+            ):
                 vehicle["card_color"] = "green"
-            elif abs(vehicle["price_with_tax"] - avg_price) >= cut_off_price:
+            elif (
+                abs(
+                    vehicle["price_with_tax"]
+                    - avg_price
+                )
+                >= cut_off_price
+            ):
                 vehicle["card_color"] = "red"
             else:
                 vehicle["card_color"] = "yellow"
 
-            vehicles.append(vehicle)#2091556
+            vehicles.append(vehicle)  # 2091556
         return {
             "session": jsonable_encoder(auth),
             "details": jsonable_encoder(vehicles),
         }
     except AuthApiError:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials",
+        )
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(
+            status_code=500, detail=str(e)
+        )
 
 
 @app.get("/get_car_comparisons")
 def get_car_comparisons(
     access_token: str,
     refresh_token: str,
-    client: Annotated[Client, Depends(get_session)],
+    client: Annotated[
+        Client, Depends(get_session)
+    ],
     car_id: str,
 ):
     try:
@@ -215,7 +336,9 @@ def get_car_comparisons(
         response = (
             client.table("comparisons")
             .select("*")
-            .order("matching_percentage", desc=True)
+            .order(
+                "matching_percentage", desc=True
+            )
             .eq("parent_car_id", car_id)
             .execute()
         )
@@ -225,30 +348,47 @@ def get_car_comparisons(
         }
 
     except AuthApiError:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials",
+        )
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(
+            status_code=500, detail=str(e)
+        )
+
 
 @app.get("/scrape_status")
 def get_status(
     access_token: str,
     refresh_token: str,
-    client: Annotated[Client, Depends(get_session)],
+    client: Annotated[
+        Client, Depends(get_session)
+    ],
 ):
     try:
         auth = client.auth.set_session(
             access_token=access_token,
             refresh_token=refresh_token,
         )
-        response = client.table("Status").select("*").execute()
+        response = (
+            client.table("Status")
+            .select("*")
+            .execute()
+        )
         return {
             "session": jsonable_encoder(auth),
             "details": jsonable_encoder(response),
         }
     except AuthApiError:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials",
+        )
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(
+            status_code=500, detail=str(e)
+        )
 
 
 @app.post("/start_scraping")
@@ -256,7 +396,9 @@ def get_start_scraping(
     access_token: str,
     refresh_token: str,
     background_task: BackgroundTasks,
-    client: Annotated[Client, Depends(get_session)],
+    client: Annotated[
+        Client, Depends(get_session)
+    ],
     dev: bool = True,
     mileage_plus_minus: int = 10000,
     ignore_old: bool = True,
@@ -267,10 +409,22 @@ def get_start_scraping(
             access_token=access_token,
             refresh_token=refresh_token,
         )
-        response = client.table("Status").select("status").eq("id", 1).execute()
-        if response.data[0]["status"] != "running":
+        response = (
+            client.table("Status")
+            .select("status")
+            .eq("id", 1)
+            .execute()
+        )
+        if (
+            response.data[0]["status"]
+            != "running"
+        ):
             background_task.add_task(
-                start_services, mileage_plus_minus, ignore_old, sites_to_scrape, dev
+                start_services,
+                mileage_plus_minus,
+                ignore_old,
+                sites_to_scrape,
+                dev,
             )
             client.table("Status").update(
                 {
@@ -291,14 +445,27 @@ def get_start_scraping(
             }
     except AuthApiError:
         client.table("Status").update(
-            {"id": 1, "status": "failed", "stopped_at": datetime.now().isoformat()}
+            {
+                "id": 1,
+                "status": "failed",
+                "stopped_at": datetime.now().isoformat(),
+            }
         ).eq("id", 1).execute()
-        raise HTTPException(status_code=401, detail="Invalid credentials")
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials",
+        )
     except Exception as e:
         client.table("Status").update(
-            {"id": 1, "status": "failed", "stopped_at": datetime.now().isoformat()}
+            {
+                "id": 1,
+                "status": "failed",
+                "stopped_at": datetime.now().isoformat(),
+            }
         ).eq("id", 1).execute()
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(
+            status_code=500, detail=str(e)
+        )
 
 
 @app.post("/upload")
@@ -313,37 +480,58 @@ async def upload_file(file: UploadFile):
         dict: Upload status and file path
     """
     # Validate file
-    if not file:
-        raise HTTPException(status_code=400, detail="No file provided")
-
+    # if not file:
+    #     raise HTTPException(status_code=400, detail="No file provided")
+    #
     # Validate file extension
-    file_extension = os.path.splitext(file.filename)[1].lower()
+    if not file.filename:
+        return None
+
+    file_extension = os.path.splitext(
+        file.filename
+    )[1].lower()
     allowed_extensions = {".xlsx", ".xls"}
     if file_extension not in allowed_extensions:
         raise HTTPException(
-            status_code=400, detail="Only Excel files (.xlsx, .xls) are allowed"
+            status_code=400,
+            detail="Only Excel files (.xlsx, .xls) are allowed",
         )
 
     try:
         # Stream the file to disk in chunks
-        async with aiofiles.open(OUT_FILE, "wb") as out_file:
-            while chunk := await file.read(256 * 256):  # Read 1MB chunks
+        async with aiofiles.open(
+            OUT_FILE, "wb"
+        ) as out_file:
+            while chunk := await file.read(
+                256 * 256
+            ):  # Read 1MB chunks
                 await out_file.write(chunk)
 
     except Exception as e:
         # Clean up if something goes wrong
         if os.path.exists(OUT_FILE):
             os.remove(OUT_FILE)
-        raise HTTPException(status_code=500, detail=f"Error uploading file: {str(e)}")
+        raise HTTPException(
+            status_code=500,
+            detail=f"Error uploading file: {str(e)}",
+        )
 
     finally:
         # Ensure the file is closed
         await file.close()
-    return {"message": "File uploaded successfully", "file_path": OUT_FILE}
+    return {
+        "message": "File uploaded successfully",
+        "file_path": OUT_FILE,
+    }
 
 
 if __name__ == "__main__":
-    start_services(10000, dev=True, ignore_old=False, sites_to_scrape=["lacentrale"])
+    start_services(
+        10000,
+        dev=True,
+        ignore_old=False,
+        sites_to_scrape=["lacentrale"],
+    )
 
 
 # Try to set up the api on the server.
