27575f4d25e452b8c350ab761123c007c4077bc8
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-11-13 18:05:56 +0100

    interface added

---
+++ interface/app/pages/home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <UPageHeader
+            title="PageHeader"
+            description="A responsive page header with title, description and actions."
+            headline="Components"
+        />
+    </div>
+</template>
+
+<script setup lang="ts"></script>
