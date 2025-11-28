cf0ae1fc01c5215ec315d3e54a0f895b764d78f3
Eldo-Moréo GBOHOUILI <eldomoreogbohouil@gmail.com>
2025-11-28 20:33:21 +0100

    ajout du bouton pour ajouter les fichiers dans le dossier upload

--- interface/nuxt.config.ts
+++ interface/nuxt.config.ts
@@ -3,10 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: [ "@nuxt/ui", "@nuxt/fonts"],
+  modules: ["@nuxt/ui", "@nuxt/fonts", "nuxt-file-storage"],
+  fileStorage: {
+    mount:
+      "/home/eldo-moreo/Project/Dev/Tech2work/auto-app-backend/public/uploads",
+  },
   runtimeConfig: {
     public: {
-      backendUrl: 'http://127.0.0.1:5000'
-    }
-  }
+      backendUrl: "http://127.0.0.1:5000",
+    },
+  },
 });
